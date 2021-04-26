8797ce92a055a67a79a859a95e6374283b23c2a9
Pavel Pavlov <igrokqq1991@ukr.net>
2021-04-26 18:55:26 +0300

    Fix spacings

--- src/components/book/types/book.type.ts
+++ src/components/book/types/book.type.ts
@@ -1,13 +1,13 @@
 type Book = {
-	readonly title: string;
-	readonly description: string;
-	readonly tags: string[];
-	readonly genres: string[];
-	readonly authors: string[];
-	readonly price: number;
-	readonly publishedAt: Date;
-	readonly language: string;
-	readonly pagesCount: number;
+  readonly title: string;
+  readonly description: string;
+  readonly tags: string[];
+  readonly genres: string[];
+  readonly authors: string[];
+  readonly price: number;
+  readonly publishedAt: Date;
+  readonly language: string;
+  readonly pagesCount: number;
   readonly weight: number;
   readonly format: string;
 };
